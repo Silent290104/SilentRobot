73389d252a80f3e294181f756432610b8c573757
Mohit <102339239+Wolf2901@users.noreply.github.com>
2022-12-04 20:22:26 +0530

    Add files via upload

---
+++ ABG/helper.py
@@ -0,0 +1,6 @@
+HELP_IMG = ["https://telegra.ph/file/653cc589cef8ce310a9f2.jpg"]
+
+START_IMG = ["https://telegra.ph/file/653cc589cef8ce310a9f2.jpg"]
+
+
+PHOTO = ["https://telegra.ph/file/653cc589cef8ce310a9f2.jpg"]
---
+++ Procfile
@@ -0,0 +1,2 @@
+#web: python3 -m Silent
+worker: python3 -m Silent
