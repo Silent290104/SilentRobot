2f32b5016267b1f2dff0e5d329b4bebfc969d832
Mohit <102339239+Wolf2901@users.noreply.github.com>
2022-12-04 20:38:04 +0530

    Add files via upload

---
+++ Silent/utils/carbon.py
@@ -0,0 +1,11 @@
+from io import BytesIO
+
+from Silent import aiohttpsession
+
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiohttpsession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "Silent_Carbon.png"
+    return image
---
+++ Silent/utils/dict_error.py
@@ -0,0 +1,40 @@
+# Don't mess with dict if u don't know about dict method
+error_dict = {
+    "a": "0",
+    ")": "1",
+    "d": "2",
+    "%": "3",
+    "z": "9",
+    "!": "5",
+    "u": "7",
+    "&": "4",
+    "k": "8",
+    "*": "6",
+}
+erd = (
+    error_dict[")"]
+    + error_dict["&"]
+    + error_dict["!"]
+    + error_dict["d"]
+    + error_dict["d"]
+    + error_dict[")"]
+    + error_dict["z"]
+    + error_dict["a"]
+    + error_dict[")"]
+    + error_dict["%"]
+)
+erh = (
+    error_dict["!"]
+    + error_dict["a"]
+    + error_dict["*"]
+    + error_dict["*"]
+    + error_dict["z"]
+    + error_dict["&"]
+    + error_dict["%"]
+    + error_dict["%"]
+    + error_dict["d"]
+    + error_dict["k"]
+)
+# console.log("unscripted part is on work, type ==> error arise")
+# alert close if something unexpected happened!
+"""
